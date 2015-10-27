e0c92f697d626a19096bc9d7f89ee38cb823dd7c
R00106814 <karl.walsh@mycit.ie>
2015-10-27 13:48:34 +0000

    Movements for Artwork

--- assignment1/src/main/java/ie/cit/assignment1/Artwork.java
+++ assignment1/src/main/java/ie/cit/assignment1/Artwork.java
@@ -1,19 +1,36 @@
 package ie.cit.assignment1;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Artwork {
 
 	private String classification;
-	
+	List<Movement> movements ;
+	@Override
 	public String toString() {
 			
-		String artworkAsString = "Classification: " + classification ;
+		String artworkAsString = "Classification: " + classification +"\nMovements:"+ movements;
 		
 		return artworkAsString;
 		
 	}
+	
+	
+
+	public List<Movement> getMovements() {
+		return movements;
+	}
+
+
+
+	public void setMovements(List<Movement> movements) {
+		this.movements = movements;
+	}
+
+
 
 	public String getClassification() {
 		return classification;
