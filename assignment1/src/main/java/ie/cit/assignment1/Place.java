6c5fd75fbe5635210b4586907c21bdcd6488d6c1
R00106814 <karl.walsh@mycit.ie>
2015-10-27 17:56:05 +0000

    Place class didn't commit initially

---
+++ assignment1/src/main/java/ie/cit/assignment1/Place.java
@@ -0,0 +1,40 @@
+package ie.cit.assignment1;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class Place {
+	
+	private String name;
+	private String placeName;
+	private String placeType;
+	
+	@Override
+    public String toString() {
+        return   name  +  " "+ placeName +" -- " + placeType ;
+    }
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPlaceName() {
+		return placeName;
+	}
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+	public String getPlaceType() {
+		return placeType;
+	}
+	public void setPlaceType(String placeType) {
+		this.placeType = placeType;
+	}
+	
+	
+
+
+}
