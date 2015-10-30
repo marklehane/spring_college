7878c1d9c1945b56033dba4d16bb75fd1569a345
David Hurley <david.j.hurley@mycit.ie>
2015-10-30 12:22:16 +0000

    added changes to save method in the jdbcartworkrepository.java

--- assignemnt1/src/main/java/ie/cit/assignment1/JdbcArtworkRepository.java
+++ assignemnt1/src/main/java/ie/cit/assignment1/JdbcArtworkRepository.java
@@ -1,8 +1,23 @@
 package ie.cit.assignment1;
 
+import java.util.List;
+import java.util.Map;
 
+import javax.sql.DataSource;
 
-public class JdbcArtworkRepository {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class JdbcArtworkRepository implements CommandLineRunner {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	public void setDataSource(DataSource dataSource) {
+	    this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
 	
 	private Artwork artwork;
 	
@@ -13,6 +28,24 @@ public class JdbcArtworkRepository {
 
 	public void save()
 	{
+	
+		// Query for a list of maps with key-value pairs
+				
+					
+				System.out.println("\nQuery 1 (List all artists using resultset Map)\n----------------");
+					
+				String sql = "SELECT * FROM artwork";
+				List<Map<String, Object>> resultSet = jdbcTemplate.queryForList(sql);
+					
+				for (Map<String, Object> row : resultSet) {
+					System.out.println("Name: " + row.get("name"));
+					System.out.println("ID: " + row.get("artwork_id"));
+					System.out.println("artist: " + row.get("artist") + "\n");
+				}
+
+
+			
+		
 	System.out.println("testing"+ artwork.getMovements() +
 	artwork.getClassification());
 		
@@ -24,6 +57,13 @@ public class JdbcArtworkRepository {
 
 	public void setArtwork(Artwork artwork) {
 		this.artwork = artwork;
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		// TODO Auto-generated method stub
+		save();
+		
 	}
 	
 	
