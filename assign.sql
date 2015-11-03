d3e7cf9fa6543cfda1a29f00800e094e9a39dfd8
Karl Walsh <karl.walsh@mycit.ie>
2015-11-03 15:33:20 +0000

    assign.sql edited

--- assign.sql
+++ assign.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
+-- version 4.5.0.2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 03, 2015 at 11:28 AM
--- Server version: 5.6.20
--- PHP Version: 5.5.15
+-- Host: localhost
+-- Generation Time: Nov 03, 2015 at 04:32 PM
+-- Server version: 10.0.17-MariaDB
+-- PHP Version: 5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `assignment1`
@@ -26,11 +26,11 @@ SET time_zone = "+00:00";
 -- Table structure for table `artists`
 --
 
-CREATE TABLE IF NOT EXISTS `artists` (
-`id` int(11) NOT NULL,
+CREATE TABLE `artists` (
+  `id` int(11) NOT NULL,
   `fullname` varchar(100) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
   `gender` varchar(10) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `artists`
@@ -42,8 +42,11 @@ INSERT INTO `artists` (`id`, `fullname`, `gender`) VALUES
 (3, 'Opie, Julian', 'male'),
 (4, 'Etty, William', 'male'),
 (5, 'Wallis, Henry', 'male'),
-(6, 'Magdalena Abakanowicz', 'Female'),
-(7, 'Lemuel Francis Abbott', 'Male');
+(6, 'Robert Delaunay', 'Male'),
+(7, 'Robert Delaunay', 'Male'),
+(8, 'Robert Delaunay', 'Male'),
+(9, 'Robert Delaunay', 'Male'),
+(992, 'Robert Delaunay', 'Male');
 
 -- --------------------------------------------------------
 
@@ -51,7 +54,7 @@ INSERT INTO `artists` (`id`, `fullname`, `gender`) VALUES
 -- Table structure for table `artist_movements`
 --
 
-CREATE TABLE IF NOT EXISTS `artist_movements` (
+CREATE TABLE `artist_movements` (
   `artist_id` int(11) NOT NULL,
   `movement_Id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -65,7 +68,9 @@ INSERT INTO `artist_movements` (`artist_id`, `movement_Id`) VALUES
 (1, 2),
 (3, 3),
 (4, 4),
-(5, 4);
+(5, 4),
+(299, 992),
+(314, 992);
 
 -- --------------------------------------------------------
 
@@ -73,11 +78,35 @@ INSERT INTO `artist_movements` (`artist_id`, `movement_Id`) VALUES
 -- Table structure for table `artworks`
 --
 
-CREATE TABLE IF NOT EXISTS `artworks` (
-`artwork_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `artist` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE `artworks` (
+  `artwork_id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `artworks`
+--
+
+INSERT INTO `artworks` (`artwork_id`, `name`) VALUES
+('A00067', 'Studies of a Sleeve');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artwork_artist`
+--
+
+CREATE TABLE `artwork_artist` (
+  `artwork_id` varchar(255) NOT NULL,
+  `artist_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `artwork_artist`
+--
+
+INSERT INTO `artwork_artist` (`artwork_id`, `artist_id`) VALUES
+('A00067', '68');
 
 -- --------------------------------------------------------
 
@@ -85,10 +114,17 @@ CREATE TABLE IF NOT EXISTS `artworks` (
 -- Table structure for table `artwork_movements`
 --
 
-CREATE TABLE IF NOT EXISTS `artwork_movements` (
-`atrwork_id` int(11) NOT NULL,
+CREATE TABLE `artwork_movements` (
+  `artwork_id` varchar(255) NOT NULL,
   `movement_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `artwork_movements`
+--
+
+INSERT INTO `artwork_movements` (`artwork_id`, `movement_id`) VALUES
+('A00067', 363);
 
 -- --------------------------------------------------------
 
@@ -96,11 +132,11 @@ CREATE TABLE IF NOT EXISTS `artwork_movements` (
 -- Table structure for table `authorities`
 --
 
-CREATE TABLE IF NOT EXISTS `authorities` (
-`authority_id` int(11) NOT NULL,
+CREATE TABLE `authorities` (
+  `authority_id` int(11) NOT NULL,
   `authority` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -108,25 +144,19 @@ CREATE TABLE IF NOT EXISTS `authorities` (
 -- Table structure for table `birth`
 --
 
-CREATE TABLE IF NOT EXISTS `birth` (
-`birth_id` int(11) NOT NULL,
-  `date` date NOT NULL,
+CREATE TABLE `birth` (
+  `birth_id` int(11) NOT NULL,
+  `date` varchar(244) NOT NULL,
+  `place` varchar(255) NOT NULL,
   `artist_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Table structure for table `comments`
+-- Dumping data for table `birth`
 --
 
-CREATE TABLE IF NOT EXISTS `comments` (
-`comment_id` int(11) NOT NULL,
-  `artwork_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `comment` text NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+INSERT INTO `birth` (`birth_id`, `date`, `place`, `artist_id`) VALUES
+(992, '1885', 'Paris', 992);
 
 -- --------------------------------------------------------
 
@@ -134,10 +164,10 @@ CREATE TABLE IF NOT EXISTS `comments` (
 -- Table structure for table `movements`
 --
 
-CREATE TABLE IF NOT EXISTS `movements` (
-`id` int(11) NOT NULL,
+CREATE TABLE `movements` (
+  `id` int(11) NOT NULL,
   `name` varchar(100) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `movements`
@@ -147,7 +177,10 @@ INSERT INTO `movements` (`id`, `name`) VALUES
 (1, 'Geometry of Fear'),
 (2, 'Kinetic Art'),
 (3, 'New British Sculpture'),
-(4, 'Romanticism');
+(4, 'Romanticism'),
+(299, 'Cubism'),
+(314, 'Orphic Cubism'),
+(363, 'Pre-Raphaelite Brotherhood');
 
 -- --------------------------------------------------------
 
@@ -155,11 +188,11 @@ INSERT INTO `movements` (`id`, `name`) VALUES
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-`user_id` int(11) NOT NULL,
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
   `password` varchar(255) NOT NULL,
   `username` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -169,55 +202,55 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Indexes for table `artists`
 --
 ALTER TABLE `artists`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `artist_movements`
 --
 ALTER TABLE `artist_movements`
- ADD PRIMARY KEY (`artist_id`,`movement_Id`);
+  ADD PRIMARY KEY (`artist_id`,`movement_Id`);
 
 --
 -- Indexes for table `artworks`
 --
 ALTER TABLE `artworks`
- ADD PRIMARY KEY (`artwork_id`);
+  ADD PRIMARY KEY (`artwork_id`);
+
+--
+-- Indexes for table `artwork_artist`
+--
+ALTER TABLE `artwork_artist`
+  ADD PRIMARY KEY (`artwork_id`,`artist_id`);
 
 --
 -- Indexes for table `artwork_movements`
 --
 ALTER TABLE `artwork_movements`
- ADD PRIMARY KEY (`atrwork_id`);
+  ADD PRIMARY KEY (`artwork_id`);
 
 --
 -- Indexes for table `authorities`
 --
 ALTER TABLE `authorities`
- ADD PRIMARY KEY (`authority_id`);
+  ADD PRIMARY KEY (`authority_id`);
 
 --
 -- Indexes for table `birth`
 --
 ALTER TABLE `birth`
- ADD PRIMARY KEY (`birth_id`);
-
---
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
- ADD PRIMARY KEY (`comment_id`);
+  ADD PRIMARY KEY (`birth_id`);
 
 --
 -- Indexes for table `movements`
 --
 ALTER TABLE `movements`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
- ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -227,42 +260,27 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `artists`
 --
 ALTER TABLE `artists`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `artworks`
---
-ALTER TABLE `artworks`
-MODIFY `artwork_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `artwork_movements`
---
-ALTER TABLE `artwork_movements`
-MODIFY `atrwork_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=993;
 --
 -- AUTO_INCREMENT for table `authorities`
 --
 ALTER TABLE `authorities`
-MODIFY `authority_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `authority_id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `birth`
 --
 ALTER TABLE `birth`
-MODIFY `birth_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `comments`
---
-ALTER TABLE `comments`
-MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `birth_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=993;
 --
 -- AUTO_INCREMENT for table `movements`
 --
 ALTER TABLE `movements`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=364;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
