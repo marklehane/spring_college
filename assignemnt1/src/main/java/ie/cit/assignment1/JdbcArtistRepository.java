d7f0f782225e36d4d42324c41c529bc5d34582b1
jvodwaterville <john.v.odwyer@mycit.ie>
2015-11-03 16:49:21 +0000

    fixed adding artist_movements

--- assignemnt1/src/main/java/ie/cit/assignment1/JdbcArtistRepository.java
+++ assignemnt1/src/main/java/ie/cit/assignment1/JdbcArtistRepository.java
@@ -89,7 +89,7 @@ public class JdbcArtistRepository implements ArtistDao {
 			String insertSql2 ;
 			insertSql2 ="insert into artist_movements values(?,?)";
 			try {
-	        jdbcTemplate.update(insertSql2,new Object[]{m.getId(),artistId});
+	        jdbcTemplate.update(insertSql2,new Object[]{artistId, m.getId()});
 			}
 			catch(Exception e)
 			    {
