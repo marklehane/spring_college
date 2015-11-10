4cc0448896dcd49b157316322c5ae6a1676dcea5
R00106814 <karl.walsh@mycit.ie>
2015-11-10 09:56:03 +0000

    thymeleaf up and running

---
+++ assignment1/src/main/java/TestApplication/indexController.java
@@ -0,0 +1,14 @@
+package TestApplication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+
+public class indexController {
+	
+@RequestMapping("/")
+String index(){
+	return "index";
+}
+
+}
